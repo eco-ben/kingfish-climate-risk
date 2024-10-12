99b534ae3f537a5283a1eb4f8f078729741c09df
Ben Grier <grierben777@gmail.com>
2024-10-12 16:14:41 +1000

    Add mapping and PCA plots to quarto and cleanup/tab the plots for render

--- mCRS pipeline Scripts/results_analysis_functions.R
+++ mCRS pipeline Scripts/results_analysis_functions.R
@@ -57,3 +57,134 @@ mean_Delta_temperature = function(x){
   
   return(all_scens)
 }
+
+PCA_plotting = function(dist, env, scenario, env_startcol, env_endcol, refyear_start, refyear_end, plot_title, method=""){
+  all_pca = data.frame()
+  all_centroids = data.frame()
+  all_polys = data.frame()
+  
+  env <- as.data.frame(env)
+  env <- env[!env$Extract.ID %in% c('103', '204', '200', '111', '112','34','52'),] # ID locations removed as of thesis writing (not including NWP loc)
+  env$Scenario <- ifelse(env$Date >= '2001-01-01' & env$Date <= '2014-12-31', scenario, env$Scenario)
+  env <- left_join(env, coordinates[,c(9,1,2)], by = c('Extract.ID'))
+  env.prcomp <- env[env$Date >= refyear_start &
+                      env$Date <= refyear_end, 
+                    c(env_startcol:env_endcol)]#environmental conditions for the reference period
+  env.prcomp <- prcomp(env.prcomp, scale=T) # summary prcomp - 71+% - Ensemble proportion of variance explained by the first 2 PC axes
+  env.prcomp.data <- as.data.frame(predict(object=env.prcomp,
+                                           newdata=env[,c(env_startcol:env_endcol)]))# rotated environmental data
+  env.prcomp.data <- env.prcomp.data[,c(1:2)] #only use PC1 and PC2
+  env.prcomp.data$Extract.ID <- env$Extract.ID 
+  env.prcomp.data$Date <- env$Date
+  env.prcomp.data$Scenario <- unlist(env$Scenario)
+  env.prcomp.data$Month <- as.numeric(format(env.prcomp.data$Date,'%m'))
+  env.prcomp.data$Depth <- unlist(env$Depth)
+  env.prcomp.data <- env.prcomp.data[,c(3:5,7,1,2)]
+  env.prcomp.data <- env.prcomp.data[complete.cases(env.prcomp.data),] #remove invomplete rows 
+  sp <- unique(dist$PopID)
+  all_pca <- env.prcomp.data
+  all_pca = left_join(all_pca, dist, by='Extract.ID')
+  polygons <- vector('list',length=length(sp))
+  pc.centr <- vector('list',length = length(sp))
+  for (m in 1:length(sp)) {
+    tmp <- unique(dist[dist$PopID == sp[[m]],]) #IDs for relevant population
+    tmp.data <- sqldf::sqldf("SELECT * from tmp LEFT JOIN
+                               'env.prcomp.data' USING (`Extract.ID`)") #join tmp to PC results -> tmp.data
+    tmp.data <- tmp.data[complete.cases(tmp.data),] #remove incomplete results
+    env.coords <- droplevels(tmp.data[tmp.data$Date >= refyear_start & tmp.data$Date <= refyear_end, ]) #include only the reference period and relevant population
+    #env.coords <- env.coords[chull(env.coords[,5], env.coords[,6]),] #create convex hull around condition points
+    
+    env.poly <- distfree.cr(env.coords[,c("PC1","PC2")],alpha = 0.05, draw = F)
+    env.poly.pts <- data.frame(env.poly$polygon)
+    FL <- env.poly.pts[1,]
+    env.poly.pts <- rbind(env.poly.pts, FL)
+    env.poly <- sp::SpatialPolygons(list(
+      sp::Polygons(list(sp::Polygon(env.poly.pts)), #the polygon used is the convex hull of environmental points
+                   'historical')))
+    env.polygon_sf <- sf::st_as_sf(env.poly, coords = env.polygon@polygons[[1]]@Polygons[[1]]@coords)
+    env.polygon_sf$PopID <- unlist(sp[m])
+    
+    if(method == "surface"){
+      spat.pcs = Full[Full$PopID == sp[m] & Full$Date <= refyear_end & Full$Depth == unique(env$Depth),]
+    }
+    else{
+      spat.pcs <- Full[Full$PopID == sp[m] & Full$Date <= refyear_end,]
+    }
+
+    spat.pcs <- spat.pcs[complete.cases(spat.pcs),]
+    env.centroid <- c(mean(spat.pcs$pred.PC1),mean(spat.pcs$pred.PC2))
+    env.centroid <- data.frame(env.centroid[1],env.centroid[2])
+    env.centroid$PopID = sp[m]
+    all_polys = rbind(all_polys, env.polygon_sf)
+    all_centroids = rbind(all_centroids, env.centroid)
+  }
+  
+  inset <- ggplot() +
+    geom_segment(data=as.data.frame(env.prcomp$rotation),
+                 aes(x=0, y=0, xend=(PC1), yend=(PC2)),
+                 arrow=arrow(length=unit(0.01, 'cm')), color='coral1') +
+    annotate(geom='text',
+             x=(as.data.frame(env.prcomp$rotation)[1,1]),
+             y=(as.data.frame(env.prcomp$rotation)[1,2]),
+             size=4,
+             label=paste0((row.names(env.prcomp$rotation)[1]))) +
+    annotate(geom='text',
+             x=(as.data.frame(env.prcomp$rotation)[2,1]),
+             y=(as.data.frame(env.prcomp$rotation)[2,2]),
+             size=4,
+             label=paste0((row.names(env.prcomp$rotation)[2]))) +
+    annotate(geom='text',
+             x=(as.data.frame(env.prcomp$rotation)[3,1]),
+             y=(as.data.frame(env.prcomp$rotation)[3,2]),
+             size=4,
+             label=paste0((row.names(env.prcomp$rotation)[3]))) +
+    annotate(geom='text',
+             x=(as.data.frame(env.prcomp$rotation)[4,1]),
+             y=(as.data.frame(env.prcomp$rotation)[4,2]),
+             size=4,
+             label=paste0((row.names(env.prcomp$rotation)[4]))) +
+    theme_void() + scale_x_continuous(limits = c((min(as.data.frame(env.prcomp$rotation)$PC1)-1.5), 
+                                                 (max(as.data.frame(env.prcomp$rotation)$PC2)+1.5))) + 
+    scale_y_continuous(limits = c((min(as.data.frame(env.prcomp$rotation)$PC2)-1.5), 
+                                  (max(as.data.frame(env.prcomp$rotation)$PC2)+1.5))) 
+  
+  all_pca$Year = as.numeric(format(as.Date(all_pca$Date,'%Y-m-%d'),'%Y'))
+  year_pca = all_pca %>% group_by(Extract.ID,Depth, PopID, Scenario, Year) %>%
+    summarise(PC1 = mean(PC1), PC2 = mean(PC2))
+  
+  pca_plot = ggplot() + 
+    geom_point(data = year_pca,
+               aes(x = PC1, y = PC2, color = as.factor(Depth)),alpha = 0.4) + 
+    geom_point(data = all_centroids,aes(x = env.centroid.1., y = env.centroid.2.), color = 'black', size = 1, alpha = 0.7) +
+    geom_sf(data = all_polys, alpha =0.3) +
+    annotation_custom(grob = ggplotGrob(inset))+
+    theme_classic() +
+    theme(legend.position = 'none') + 
+    facet_wrap(~PopID) + ggtitle(paste0(plot_title)) + theme(axis.text = element_text(size = 14), 
+                                                                                axis.title = element_text(size = 17, face = 'bold'), 
+                                                                                strip.text.x = element_text(size = 12)) 
+  return(pca_plot)
+}
+
+DeltamCRSMap <- function(Scenario){
+  Pacific <- ggplot() + 
+    geom_sf(data =  Warmlevel.data_360[Warmlevel.data_360$Scenario == Scenario,], 
+            aes(color = DeltaCRS), size = 1.2, alpha = 0.6) +
+    geom_sf(data = World_360) +
+    geom_sf(data = Boxes, fill = NA, linewidth = 0.2) +
+    #ggtitle('DeltaCRS - 10m') + 
+    theme_void() + theme(legend.position = "none") + theme(panel.background = element_rect(fill = 'white')) +
+    scale_color_scico(palette = 'batlow', midpoint = 0, limits = c(minDcrs, maxDcrs)) + 
+    coord_sf(xlim = c(100,300),ylim = c(50,-60)) + facet_wrap(~Depth)
+  SA <- ggplot() + 
+    geom_sf(data =  all_depths_sf[all_depths_sf$Scenario == Scenario,], 
+            aes(color = DeltaCRS), size = 1.2, alpha = 0.6) +
+    geom_sf(data = World) +
+    theme_void() + theme(legend.position = "none") + theme(panel.background = element_rect(fill = 'white')) + 
+    scale_color_scico(palette = 'batlow', midpoint = 0, limits = c(minDcrs, maxDcrs)) + 
+    coord_sf(xlim = c(10,40),ylim = c(-20,-40)) + facet_wrap(~Depth)
+  final <- ggarrange(Pacific, SA, heights = c(3,1), widths = c(2.5,1), labels = c('(a)','(b)'), vjust = 1)
+  final <- ggarrange(final,leg, ncol = 1, heights = c(6,1), widths = c(6,1))
+
+  return(final)
+}
